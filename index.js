d60b11746985bc25c1ad09eda1e3bda65c5b827a
malex <mircea.alexandru@gmail.com>
2016-03-03 23:32:42 +0200

    use seneca-web for registering redirect google login

--- index.js
+++ index.js
@@ -9,13 +9,32 @@ module.exports = function (options) {
     .use('auth', {restrict: '/api'})
     .use('mesh', {auto: true})
 
-  seneca.route({
-    method: 'GET',
-    path: '/auth/google',
-    handler: function (request, reply) {
-      reply.redirect('http://localhost:3050/auth/google?callback_url=http://localhost:3000')
+
+  function redirectGoogle(args, done){
+    return done(
+      null,
+      {
+        http$:
+        {
+          redirect: 'http://localhost:3050/auth/google?callback_url=http://localhost:3000'
+        }
+      }
+    )
+  }
+
+  seneca
+    .add('role: concorda, cmd: redirectGoogle', redirectGoogle)
+
+  seneca.act({
+    role: 'web', use: {
+      name: 'concorda',
+      prefix: '/auth',
+      pin: {role: 'concorda', cmd: '*'},
+      map: {
+        redirectGoogle: {GET: true, alias: 'google'}
+      }
     }
-  });
+  })
 
   return {
     name: name
